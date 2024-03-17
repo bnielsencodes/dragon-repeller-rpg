bceab93ecf26502ea4b0121270befe75e644dcce
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-03-17 00:40:03 -0700

    Create lose, winGame, and restart functions

--- index.js
+++ index.js
@@ -241,3 +241,23 @@ function defeatMonster() {
   xpText.innerText = xp;
   update(locations[4]);
 }
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
