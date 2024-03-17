483b3671f2ccadd8e1986994eeda6a512efcf025
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-03-17 00:37:10 -0700

    Create fightSlime, fightBeast, and fightDragon functions

--- index.js
+++ index.js
@@ -168,3 +168,18 @@ function sellWeapon() {
     text.innerText = "Don't sell your only weapon!";
   }
 }
+
+function fightSlime() {
+  fighting = 0;
+  goFight();
+}
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
