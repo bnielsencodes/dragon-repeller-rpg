ec31e69ad2cb1305e586c7e28cd3c70039e95102
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-03-17 00:37:20 -0700

    Create goFight function

--- index.js
+++ index.js
@@ -183,3 +183,11 @@ function fightDragon() {
   fighting = 2;
   goFight();
 }
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
